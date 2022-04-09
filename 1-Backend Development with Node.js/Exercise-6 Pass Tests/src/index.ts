125dbaa0f4acac4f7fa8f25c18c41d3715229025
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-04-09 21:49:57 +0200

    added starter files for 'Exercise-6 Pass Test'

---
+++ 1-Backend Development with Node.js/Exercise-6 Pass Tests/src/index.ts
@@ -0,0 +1,17 @@
+import arrays from './utilities/arrays.js';
+import numbers from './utilities/numbers.js';
+import strings from './utilities/strings.js';
+
+const numArr = [3, 4, 5, 6];
+const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
+const arrSum = arrays.addArr(numArr);
+const mixArr = arrays.concatArr(numArr, wordArr);
+const myNum = ('15' as unknown) as number % 2;
+const five = parseInt('5');
+
+
+console.log(arrays.cut3(mixArr));
+console.log(numbers.sum(arrSum, myNum));
+console.log(strings.capitalize('the quick brown fox'));
+console.log(numbers.multiply(five, 8));
+console.log(arrays.lgNum(mixArr));
---
+++ 1-Backend Development with Node.js/Exercise-6 Pass Tests/src/utilities/arrays.ts
@@ -0,0 +1,39 @@
+// Concatenate two arrays
+
+const concatArr = (arr1: (string | number)[], arr2: (string | number)[]):(string | number)[] => {
+  return [...arr1, ...arr2];
+};
+
+// Add numbers in an array
+
+const addArr = (arr: number[]): number => {
+  let total = 0;
+  arr.forEach((x) => {
+    total += x;
+  });
+  return total;
+};
+
+// Find the largest number in an array
+const lgNum = (arr: (string | number)[]): number => {
+  let largest = 0 as number;
+  arr.forEach((x) => {
+    if (x > largest) {
+      largest = x as number;
+    }
+  });
+  return largest;
+};
+
+// Remove the 3rd item from an array
+const cut3 = (arr: (string|number)[]):(string | number)[] => {
+  arr.splice(2, 1);
+  return arr;
+};
+
+export = {
+  concatArr,
+  addArr,
+  lgNum,
+  cut3
+}
