d9d31ba371c028cb2b02ee939b91541efffd5fe0
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-04-11 00:27:43 +0200

    written (getCountry and getRegionCapitals) functions in index.ts file

--- 1-Backend Development with Node.js/Exercise-7 Test Driven Development/src/index.ts
+++ 1-Backend Development with Node.js/Exercise-7 Test Driven Development/src/index.ts
@@ -8,6 +8,15 @@ import axios from "axios";
  */
 
 /** Create getCountry Function here */
+async function getCountry(name: string) {
+  const getApi = await axios(`https://restcountries.eu/rest/v2/name/${name}`);
+  const data = getApi.data[0];
+  return {
+    capital: data.capital,
+    region: data.region,
+    numericCode: data.numericCode,
+  };
+}
 
 /** Create a test for this getRegion function */
 async function getRegionCountries(regionalbloc: string) {
@@ -23,6 +32,18 @@ async function getRegionCountries(regionalbloc: string) {
 }
 
 /** Create getRegionCapitals function here */
+async function getRegionCapitals(regionalbloc: string) {
+  const countryEndpoints = await getRegionCountries(regionalbloc);
+  const promises = countryEndpoints.map((endpoint: any) =>
+    axios(`https://restcountries.eu/rest/v2/name/${endpoint}`)
+  );
+  const arr = await Promise.all(promises);
+  const capitals = [];
+  for (let i = 0; i < arr.length; i++) {
+    capitals.push(arr[i].data[0].capital);
+  }
+  return capitals;
+}
 
 export default {
   getCountry,
