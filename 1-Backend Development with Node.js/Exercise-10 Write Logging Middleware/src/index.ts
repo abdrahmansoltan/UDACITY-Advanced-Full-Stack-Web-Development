e6c2bfdcd6e2629b5a7664e920214b233381d340
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-04-12 11:06:52 +0200

    added initial files for exercise-10

---
+++ 1-Backend Development with Node.js/Exercise-10 Write Logging Middleware/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+
+
+// start the Express server
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
