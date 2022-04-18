d18b1e786fb89afbf6d6e67655dd04df94d4f768
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-04-18 11:23:28 +0200

    used image-path instead of image-index to display the image + added types

--- Projects/Image Processing API/src/index.ts
+++ Projects/Image Processing API/src/index.ts
@@ -3,12 +3,12 @@ import imgs_router from "./routes/imgs_router.route";
 import img_router from "./routes/img_router.route";
 
 const app = express();
-const PORT = 3000;
+const PORT: number = 3000;
 
 app.use("/images", imgs_router);
 app.use("/images", img_router);
 
-app.listen(PORT, () => {
+app.listen(PORT, ():void => {
   console.log(`server started at http://localhost:${PORT}/images`);
 });
 
--- Projects/Image Processing API/src/routes/img_router.route.ts
+++ Projects/Image Processing API/src/routes/img_router.route.ts
@@ -1,30 +1,22 @@
 import express from "express";
 const sharp = require("sharp");
-import fs from "fs";
-
 const path = require("path");
 const img_router = express.Router();
 
 img_router.get("/:imgName", async (req, res) => {
   // getting the input-data
-  const img_name = req.params.imgName;
-  console.log("the image name :", img_name);
-  const img_width = parseInt(req.query.width as string);
-  const img_height = parseInt(req.query.height as string);
-  console.log(img_width, img_height);
-
-  // reading "images" folder
-  const img_names = await fs.promises.readdir("images");
-  console.log(img_names); // array of the images names
-  const img_id = img_names.indexOf(img_name + ".jpg");
-
-  // getting paths of the images
-  const imgsPaths = img_names.map((img) => {
-    return path.join(__dirname, "../", "../", "images", `${img}`);
-  });
+  const img_name: string = req.params.imgName;
+  const img_width: number = parseInt(req.query.width as string);
+  const img_height: number = parseInt(req.query.height as string);
 
   // source/distination paths
-  const sourceIMG: string = imgsPaths[img_id];
+  const sourceIMG: string = path.join(
+    __dirname,
+    "../",
+    "../",
+    "images",
+    `${img_name}.jpg`
+  );
   const distinationIMG: string = path.join(
     __dirname,
     "../",
@@ -34,11 +26,11 @@ img_router.get("/:imgName", async (req, res) => {
   );
 
   // using Sharp to crop the image
-  const imgCropper = async (
+  const imgCropper: Function = async (
     src: string,
     dist: string,
-    reqWidth: number = 300,
-    reqHeight: number = 300
+    reqWidth: number = 300, // default values
+    reqHeight: number = 300 // default values
   ) => {
     await sharp(src).resize(reqWidth, reqHeight).toFormat("jpeg").toFile(dist);
   };
@@ -48,7 +40,6 @@ img_router.get("/:imgName", async (req, res) => {
     res.status(200).sendFile(sourceIMG);
   } else {
     await imgCropper(sourceIMG, distinationIMG, img_width, img_height);
-
     await res.status(200).sendFile(distinationIMG);
     // here must use "await" to make it wait for the cropping first
   }
