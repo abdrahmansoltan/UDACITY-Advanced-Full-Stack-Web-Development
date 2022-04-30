df3e26ec838f54c451141778e40dde564d9b0a19
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-04-30 18:02:08 +0200

    adusted verifyAuthToken function to supply error for no Token

--- Projects/Storefront Backend/src/helpers/tokenAuth.ts
+++ Projects/Storefront Backend/src/helpers/tokenAuth.ts
@@ -1,5 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // middleware
 const verifyAuthToken = (
@@ -9,17 +12,15 @@ const verifyAuthToken = (
 ): void => {
   try {
     const authHeader: string | undefined = req.headers.authorization;
-    const token = (authHeader as string).split(" ")[1];
-    const decoded: string | object = jwt.verify(
-      token,
-      process.env.TOKEN_SECRET as string
-    );
+    if (!authHeader) {
+      throw new Error("authorization header is not available"); // Guard clause
+    }
 
-    res.locals.userData = decoded;
+    const token = (authHeader as string).split(" ")[1];
+    jwt.verify(token, process.env.TOKEN_SECRET as string);
     next(); // !IMPORTANT
   } catch (error) {
-    res.status(401);
-    next();
+    res.status(401).json(error);
   }
 };
 
