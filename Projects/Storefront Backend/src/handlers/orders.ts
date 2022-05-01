28fce517427bff4f67de8c54b63c7dc88f51a100
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-05-01 14:54:15 +0200

    added remaining routes and methods for orders model & rhandler

--- Projects/Storefront Backend/src/handlers/orders.ts
+++ Projects/Storefront Backend/src/handlers/orders.ts
@@ -6,11 +6,29 @@ const orderStore = new Order();
 
 // ---------CRUD functions--------- //
 
-// Get all completed orders by user id
-const index = async (req: Request, res: Response) => {
+// create order
+const create = async (req: Request, res: Response) => {
   try {
-    const userId: number = parseInt(req.params.user_id);
-    const Orders: OrderType[] = await orderStore.index(userId);
+    // get user data from the request
+    const order: OrderType = {
+      product_id: req.body.product_id,
+      quantity: req.body.quantity,
+      user_id: req.body.user_id,
+      status: req.body.status,
+    };
+
+    const newOrder: OrderType = await orderStore.create(order);
+    return res.json(newOrder);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+// Get all completed orders
+const CompletedOrders = async (req: Request, res: Response) => {
+  try {
+    const Orders: OrderType[] = await orderStore.getCompletedOrders();
     return res.json(Orders);
   } catch (err) {
     res.status(400);
@@ -18,12 +36,33 @@ const index = async (req: Request, res: Response) => {
   }
 };
 
-// Get current order by user id
+// Get all Current orders
+const CurrentOrders = async (req: Request, res: Response) => {
+  try {
+    const Orders: OrderType[] = await orderStore.getCurrentOrders();
+    return res.json(Orders);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+// Get orders by user id
 const show = async (req: Request, res: Response) => {
   try {
-    const userId: number = parseInt(req.params.user_id);
-    const Order: OrderType = await orderStore.show(userId);
-    return res.json(Order);
+    const userId: number = parseInt(req.params.id);
+    const Orders: OrderType[] = await orderStore.show(userId);
+    return res.json(Orders);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+// Get all orders
+const index = async (req: Request, res: Response) => {
+  try {
+    const Orders: OrderType[] = await orderStore.index();
+    return res.json(Orders);
   } catch (err) {
     res.status(400);
     res.json(err);
@@ -33,8 +72,11 @@ const show = async (req: Request, res: Response) => {
 // each route use one model
 const ordersRoutes = (app: express.Application) => {
   // using the middleware function to Validate the order's token to Authorize him to the next action
-  app.get("/orders/current/:id", verifyAuthToken, show);
-  app.get("/orders/completed/:id", verifyAuthToken, index);
+  app.post("/orders", verifyAuthToken, create);
+  app.get("/orders", verifyAuthToken, index);
+  app.get("/orders/:id", verifyAuthToken, show);
+  app.get("/orders/current", verifyAuthToken, CurrentOrders);
+  app.get("/orders/completed", verifyAuthToken, CompletedOrders);
 };
 
 export default ordersRoutes; // to be used in the server file to have clean code
--- Projects/Storefront Backend/src/models/order.ts
+++ Projects/Storefront Backend/src/models/order.ts
@@ -3,16 +3,50 @@ import client from "../database";
 
 export type OrderType = {
   id?: number;
-  status: string;
+  product_id: number;
+  quantity?: number;
   user_id: number;
+  status: string;
 };
 
 export class Order {
-  async index(id: number): Promise<OrderType[]> {
+  async create(order: OrderType): Promise<OrderType> {
+    try {
+      const { product_id, quantity, user_id, status } = order;
+
+      const conn = await client.connect();
+      const sql = `INSERT INTO orders (product_id, quantity, user_id, status) VALUES($1, $2, $3, $4) RETURNING *`;
+      const result = await conn.query(sql, [
+        product_id,
+        quantity,
+        user_id,
+        status,
+      ]);
+      conn.release();
+      return result.rows[0];
+    } catch (err) {
+      throw new Error(`Couldn't create order, ${err}`);
+    }
+  }
+
+  // get completed orders based on status
+  async getCompletedOrders(): Promise<OrderType[]> {
     try {
       const conn: PoolClient = await client.connect();
-      const sql: string = "SELECT * FROM orders WHERE user_id=$1`";
-      const result = await conn.query(sql, [id]);
+      const sql: string = "SELECT * FROM orders WHERE status LIKE 'comp%'";
+      const result = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Couldn't get the orders, ${err}`);
+    }
+  }
+  // get current orders based on status
+  async getCurrentOrders(): Promise<OrderType[]> {
+    try {
+      const conn: PoolClient = await client.connect();
+      const sql: string = "SELECT * FROM orders WHERE status LIKE 'curr%'";
+      const result = await conn.query(sql);
       conn.release();
       return result.rows;
     } catch (err) {
@@ -20,15 +54,29 @@ export class Order {
     }
   }
 
-  async show(id: number): Promise<OrderType> {
+  // get orders based on order_id (id)
+  async show(id: number): Promise<OrderType[]> {
     try {
       const sql: string = "SELECT * FROM orders WHERE id=($1)";
       const conn: PoolClient = await client.connect();
       const result: QueryResult = await conn.query(sql, [id]);
       conn.release();
-      return result.rows[0];
+      return result.rows;
     } catch (err) {
-      throw new Error(`Couldn't find order ${id}, ${err}`);
+      throw new Error(`Couldn't find orders for user: ${id}, ${err}`);
+    }
+  }
+
+  // get all orders
+  async index(): Promise<OrderType[]> {
+    try {
+      const sql: string = "SELECT * FROM orders";
+      const conn: PoolClient = await client.connect();
+      const result: QueryResult = await conn.query(sql);
+      conn.release();
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Couldn't find orders, ${err}`);
     }
   }
 }
