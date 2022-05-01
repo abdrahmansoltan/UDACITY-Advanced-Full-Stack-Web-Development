64501b00a8d37ee8f7c3f44995395efb9943e62d
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-05-01 14:56:35 +0200

    configured test-database for testing + added test script

--- Projects/Storefront Backend/src/database.ts
+++ Projects/Storefront Backend/src/database.ts
@@ -4,14 +4,34 @@ import { Pool } from "pg";
 dotenv.config(); // // initialize the environment variables
 
 // getting the information needed to connect to the database
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
-  process.env;
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_TEST_DB,
+  ENV,
+} = process.env;
 
-const client = new Pool({
-  host: POSTGRES_HOST,
-  database: POSTGRES_DB,
-  user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-});
+let client: any;
+console.log(ENV);
+
+if (ENV === "dev") {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+}
+
+if (ENV === "test") {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+}
 
 export default client;
