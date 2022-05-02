d8b2cf5e800f9b82985a7a3077951bd55189c21c
Abdelrahman Soltan <abdrahmansoltan98@gmail.com>
2022-05-02 19:20:38 +0200

    fixed type any for pg-pool connection

--- Projects/Storefront Backend/src/database.ts
+++ Projects/Storefront Backend/src/database.ts
@@ -13,25 +13,13 @@ const {
   ENV,
 } = process.env;
 
-let client;
 console.log(ENV);
 
-if (ENV === "dev") {
-  client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-  });
-}
-
-if (ENV === "test") {
-  client = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_TEST_DB,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-  });
-}
+let client: Pool = new Pool({
+  host: POSTGRES_HOST,
+  database: ENV === "dev" ? POSTGRES_DB : POSTGRES_TEST_DB,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+});
 
 export default client;
